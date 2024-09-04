f4a03d4efe4646e91e02d6d5a163f50ee57ea72e
RishabJha134 <rishabjha134@gmail.com>
2024-09-04 23:18:04 +0530

    update backend code

--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,16 +1,12 @@
 // routes/postRoutes.js
-
 const express = require('express');
 const router = express.Router();
-const { getAllPosts, getPostById, createPost, deletePost, updatePost } = require('../models/postModel');
+const { getAllPosts, getPostById, createPost, deletePost } = require('../models/postModel');
 
-// Existing routes...
+// Define routes
 router.get('/posts', getAllPosts);
 router.get('/posts/:id', getPostById);
 router.post('/posts', createPost);
 router.delete('/posts/:id', deletePost);
-
-// New route for updating a post
-router.put('/posts/:id', updatePost);
 
 module.exports = router;
