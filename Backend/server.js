948ce699d12902ee57371d0a4ab8af07866ecba5
RishabJha134 <rishabjha134@gmail.com>
2024-09-04 22:30:10 +0530

    dotenv

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,12 @@
-// server.js
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const postRoutes = require('./routes/postRoutes');
 
 const app = express();
 app.use(cors({
-  origin:"*",
+  origin: "*",
 }));
 app.use(express.json());
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/api', postRoutes);
 
 const PORT = process.env.PORT || 5000;
+console.log(process.env.PORT)
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
